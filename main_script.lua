7fc7630a54afb72da55165ab581deac4017327fb
xynxzzz <sob263179@gmail.com>
2025-07-21 16:44:23 +0300

    Update main_script.lua

--- main_script.lua
+++ main_script.lua
@@ -9,5 +9,11 @@ local v = 999
 print(z+o+v)
 
 
+if tostring(identifyexecutor()):lower():find("delta") then
+  game.Players.LocalPlayer:Kick("Injector is not supported! \\ Инжектор не поддерживаеться. Скачайте другой (к примеру Fluxus, CodeX, ArceusX)")
+end
+
+
+
 
 loadstring(game:HttpGet("https://pastefy.app/UuLeXbzb/raw"))()
